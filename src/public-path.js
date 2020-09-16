b95c96b5f086000aa207a96139774e8b1dc80a6b
musistudio <musiteam@musiiot.top>
2020-09-16 13:38:08 +0800

    init

---
+++ src/public-path.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: jinhui.li
+ * @Date: 2020-09-16 09:42:46
+ * @LastEditors: jinhui.li
+ * @LastEditTime: 2020-09-16 09:43:03
+ * @Description: 修改publicPath
+ */
+if (window.__POWERED_BY_QIANKUN__) {
+  // eslint-disable-next-line no-undef
+  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
+}
