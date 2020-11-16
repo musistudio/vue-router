3de468a30020d3b93dcd70c472c7501383fb1c30
musistudio <musiteam@musiiot.top>
2020-11-16 16:16:30 +0800

    add xbb-sdk

--- src/main.js
+++ src/main.js
@@ -2,20 +2,22 @@
  * @Author: jinhui.li
  * @Date: 2020-09-15 18:48:31
  * @LastEditors: jinhui.li
- * @LastEditTime: 2020-09-16 09:46:20
+ * @LastEditTime: 2020-11-16 16:16:01
  * @Description: 适配乾坤
  */
 import './public-path';
 import Vue from 'vue'
 import App from './App.vue'
+import xbb from 'xbb-sdk'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$xbb = xbb
 
 let instance;
 
 function render(props = {}) {
   const { container } = props;
-
   instance = new Vue({
     render: h => h(App),
   }).$mount(container ? container.querySelector('#app') : '#app');
@@ -25,33 +27,13 @@ if (!window.__POWERED_BY_QIANKUN__) {
   render();
 }
 
-function storeTest(props) {
-  props.onGlobalStateChange &&
-    props.onGlobalStateChange(
-      (value, prev) => console.log(`[onGlobalStateChange - ${props.name}]:`, value, prev),
-      true,
-    );
-  props.setGlobalState &&
-    props.setGlobalState({
-      ignore: props.name,
-      user: {
-        name: props.name,
-      },
-    });
-}
-
-export async function bootstrap() {
-  console.log('[vue] vue app bootstraped');
-}
-
-export async function mount(props) {
-  console.log('[vue] props from main framework', props);
-  storeTest(props);
-  render(props);
-}
+export const bootstrap = xbb.bootstrap;
+export const mount = xbb.mount;
+xbb.ready(render);
 
 export async function unmount() {
   instance.$destroy();
   instance.$el.innerHTML = '';
   instance = null;
+  return Promise.resolve();
 }
