ee3ea1936408bda587ff9690a857ce56ec5df03d
musistudio <musiteam@musiiot.top>
2020-11-25 09:57:04 +0800

    feat: add lint

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@
  * @LastEditTime: 2020-11-16 16:16:01
  * @Description: 适配乾坤
  */
-import './public-path';
+import './public-path'
 import Vue from 'vue'
 import App from './App.vue'
 import xbb from 'xbb-sdk'
@@ -14,26 +14,26 @@ Vue.config.productionTip = false
 
 Vue.prototype.$xbb = xbb
 
-let instance;
+let instance
 
-function render(props = {}) {
-  const { container } = props;
+function render (props = {}) {
+  const { container } = props
   instance = new Vue({
-    render: h => h(App),
-  }).$mount(container ? container.querySelector('#app') : '#app');
+    render: h => h(App)
+  }).$mount(container ? container.querySelector('#app') : '#app')
 }
 
 if (!window.__POWERED_BY_QIANKUN__) {
-  render();
+  render()
 }
 
-export const bootstrap = xbb.bootstrap;
-export const mount = xbb.mount;
-xbb.ready(render);
+export const bootstrap = xbb.bootstrap
+export const mount = xbb.mount
+xbb.ready(render)
 
-export async function unmount() {
-  instance.$destroy();
-  instance.$el.innerHTML = '';
-  instance = null;
-  return Promise.resolve();
+export async function unmount () {
+  instance.$destroy()
+  instance.$el.innerHTML = ''
+  instance = null
+  return Promise.resolve()
 }
--- src/public-path.js
+++ src/public-path.js
@@ -2,10 +2,10 @@
  * @Author: jinhui.li
  * @Date: 2020-09-16 09:42:46
  * @LastEditors: jinhui.li
- * @LastEditTime: 2020-09-16 09:43:03
+ * @LastEditTime: 2020-11-25 09:56:31
  * @Description: 修改publicPath
  */
 if (window.__POWERED_BY_QIANKUN__) {
-  // eslint-disable-next-line no-undef
-  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
+  // eslint-disable-next-line
+  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__
 }
